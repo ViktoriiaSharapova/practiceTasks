dd3e33a6ff254282aef3cac6bd8d28c18b4a9221
ViktoriiaSharapova <sharapova.viktoriia@chnu.edu.ua>
2025-12-07 18:30:56 +0200

    2

--- courseSection/m02_variables_datatypes_concatenation/Primitives/src/Task.java
+++ courseSection/m02_variables_datatypes_concatenation/Primitives/src/Task.java
@@ -1,7 +1,25 @@
 public class Task {
     public static void main(String[] args) {
 
-        //Write your solution here
+        byte age = 30;
+        short temperature = -5;
+        int distance = 1200;
+        long universeAge = 13800000000L;
+        float height = 5.8f;
+        double pi = 3.14159;
+        boolean isJavaFun = true;
+        char initial = 'G';
+
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Distance: " + distance);
+        System.out.println("Universe Age: " + universeAge);
+        System.out.println("Height: " + height);
+        System.out.println("Pi: " + pi);
+        System.out.println("Is Java Fun: " + isJavaFun);
+        System.out.println("Initial: " + initial);
+
+
 
     }
 }
